1f6d15b176fe940c6a4d8c75a131fac8441420fe
Hash Av <me@nowhere.com>
2013-01-21 23:56:49 +0100

    Turn into class

---
+++ lib/say.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby-local-exec
+
+require 'fileutils'
+require 'digest/sha2'
+require 'cgi'
+require 'logger'
+
+class AudioSample
+  def initialize(opts)
+    @message = opts[:message]
+    @lang = opts[:lang] ||= "en"
+    @dir = opts[:folder] ||= "#{ENV['HOME']}/Documents/AudioSamples"
+
+    @filename = digest_filename
+    @filepath = "#{@dir}/#{@filename}"
+
+
+    log_file = File.dirname(__FILE__) + '/../log/say.log'
+    @log = Logger.new(log_file)
+    sanity_checks
+    download unless File.exists? @filepath
+  end
+
+  def play
+    @log.info { "Playing: #{@filepath}" }
+    system %Q_mplayer "#{@filepath}" >/dev/null 2>&1_
+  end
+
+  private
+
+  def digest_filename
+    "#{Digest::SHA256.hexdigest(@message)}.mp3"
+  end
+
+  def download
+    query = CGI.escape(@message)
+    url = "http://translate.google.com/translate_tts?tl=#{@lang}&q=#{query}"
+    cmd = %Q_mplayer -user-agent "Mozilla/5.0" "#{url}" -dumpstream -dumpfile "#{@filepath}" >/dev/null 2>&1_
+    @log.info { "Message downloaded: #{@message}." }
+    system cmd
+  end
+
+  def sanity_checks
+    unless Dir.exists? @dir
+      FileUtils.mkdir_p @dir
+      @log.info { "Directory created: #{@dir}." }
+    end
+  end
+end
